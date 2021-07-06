5621301a31d01fd556c2c2173e2829056550c5ad
seldon1000 <seldon1000@tutanota.com>
2021-07-06 19:55:01 +0200

    Fixed bottom bar color in 'ChangePin' screen I missed that

--- app/src/main/java/eu/seldon1000/nextpass/ui/screens/ChangePin.kt
+++ app/src/main/java/eu/seldon1000/nextpass/ui/screens/ChangePin.kt
@@ -23,7 +23,6 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -189,7 +188,7 @@ fun ChangePin(change: Boolean) {
             )
         }
     }, bottomBar = {
-        BottomAppBar(backgroundColor = Color.Black, cutoutShape = CircleShape) {
+        BottomAppBar(cutoutShape = CircleShape) {
             IconButton(onClick = {
                 if (confirm) {
                     confirm = false
