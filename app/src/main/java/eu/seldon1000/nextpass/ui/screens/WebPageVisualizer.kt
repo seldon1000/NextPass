3a071d8dded2ec5410b23b2039579bc788adf31b
seldon1000 <seldon1000@tutanota.com>
2021-07-15 11:43:57 +0200

    added bottom bar in webview There's a button to refresh the page now.

--- app/src/main/java/eu/seldon1000/nextpass/ui/screens/WebPageVisualizer.kt
+++ app/src/main/java/eu/seldon1000/nextpass/ui/screens/WebPageVisualizer.kt
@@ -21,22 +21,66 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import eu.seldon1000.nextpass.R
+import eu.seldon1000.nextpass.ui.MainViewModel
+import eu.seldon1000.nextpass.ui.layout.MyScaffoldLayout
+import eu.seldon1000.nextpass.ui.theme.colors
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun WebPageVisualizer(urlToRender: String) {
-    AndroidView(
-        factory = {
-            WebView(it).apply {
-                settings.safeBrowsingEnabled = true
-                settings.javaScriptEnabled = true
-                settings.cacheMode = WebSettings.LOAD_NO_CACHE
-                webViewClient = WebViewClient()
-                loadUrl(urlToRender)
+    val context = LocalContext.current
+
+    val webView by remember {
+        mutableStateOf(value = WebView(context).apply {
+            settings.safeBrowsingEnabled = true
+            settings.javaScriptEnabled = true
+            settings.cacheMode = WebSettings.LOAD_NO_CACHE
+            webViewClient = WebViewClient()
+            loadUrl(urlToRender)
+        })
+    }
+
+    MyScaffoldLayout(fab = {
+        FloatingActionButton(onClick = { webView.loadUrl(urlToRender) }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_round_refresh_24),
+                contentDescription = "refresh",
+                tint = colors!!.onBackground
+            )
+        }
+    }, bottomBar = {
+        BottomAppBar(
+            cutoutShape = CircleShape,
+            modifier = Modifier.clip(shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+        ) {
+            IconButton(onClick = { MainViewModel.popBackStack() }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_round_back_arrow_24),
+                    contentDescription = "back"
+                )
             }
-        }, modifier = Modifier.fillMaxSize()
-    )
+        }
+    }) {
+        AndroidView(
+            factory = { webView },
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
